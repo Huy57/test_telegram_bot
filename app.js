1284fd62a807a8611213dc37be72ea7078fe3491
Cao Hữu Huy <huy45623@gmail.com>
2024-08-26 17:07:19 +0700

    up

--- app.js
+++ app.js
@@ -43,6 +43,24 @@ app.post("/webhook", (req, res) => {
     });
 });
 
+app.post("/webhook/interactions", (req, res) => {
+    const requestBody = req.body;
+    console.log("requestBody", requestBody)
+
+    // Xử lý yêu cầu PING từ Discord
+    if (req.body.type === 1) {
+        return res.status(200).json({ type: 1 });
+    }
+
+    // Xử lý các loại yêu cầu khác (như các tương tác từ người dùng)
+    res.status(200).json({
+        type: 4,
+        data: {
+            content: "This is a response to your command!"
+        }
+    });
+});
+
 // function verifyRequest(signature, timestamp, body) {
 //     const message = Buffer.from(timestamp + body);
 //     const signatureBuffer = Buffer.from(signature, 'hex');
